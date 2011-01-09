f75bffe9c20ad105d2200e4c12e74cf23037afd9
Mick Killianey <mk@cs.stanford.edu>
2011-01-09 21:02:33 +0000

    Add collections matchers

---
+++ collections/collections.go
@@ -0,0 +1,110 @@
+package collections
+
+import (
+	"hamcrest"
+	"reflect"
+)
+
+type _ElemAndLen interface {
+	Elem(i int) reflect.Value
+	Len() int
+}
+
+func AnyElement(matcher *hamcrest.Matcher) *hamcrest.Matcher {
+	description := hamcrest.NewDescription("AnyElement[%v]", matcher)
+	match := func(actual interface{}) *hamcrest.Result {
+		v := reflect.NewValue(actual)
+		if value, ok := v.(_ElemAndLen); ok {
+			n := value.Len()
+			for i := 0; i < n; i++ {
+				elem := value.Elem(i).Interface()
+				result := matcher.Match(elem)
+				if result.Matched() {
+					why := hamcrest.NewDescription(
+						"Matched element %v of %v: %v", i+1, n, elem)
+					return hamcrest.NewResult(true, why)
+				}
+			}
+			why := hamcrest.NewDescription(
+				"Matched none of the %v elements", n)
+			return hamcrest.NewResult(false, why)
+		}
+		return matcher.Match(v)
+	}
+	return hamcrest.NewMatcher(description, match)
+}
+
+func EveryElement(matcher *hamcrest.Matcher) *hamcrest.Matcher {
+	description := hamcrest.NewDescription("EveryElement[%v]", matcher)
+	match := func(actual interface{}) *hamcrest.Result {
+		v := reflect.NewValue(actual)
+		if value, ok := v.(_ElemAndLen); ok {
+			n := value.Len()
+			for i := 0; i < n; i++ {
+				elem := value.Elem(i).Interface()
+				result := matcher.Match(elem)
+				if !result.Matched() {
+					why := hamcrest.NewDescription(
+						"Failed to match element %v of %v: %v", i+1, n, elem)
+					return hamcrest.NewResult(false, why)
+				}
+			}
+			why := hamcrest.NewDescription(
+				"Matched all of the %v elements", n)
+			return hamcrest.NewResult(true, why)
+		}
+		return matcher.Match(v)
+	}
+	return hamcrest.NewMatcher(description, match)
+}
+
+func AnyMapElement(matcher *hamcrest.Matcher) *hamcrest.Matcher {
+	description := hamcrest.NewDescription("AnyMapElement[%v]", matcher)
+	match := func(actual interface{}) *hamcrest.Result {
+		v := reflect.NewValue(actual)
+		if value, ok := v.(*reflect.MapValue); ok {
+			keys := value.Keys()
+			for i, key := range value.Keys() {
+				elem := value.Elem(key).Interface()
+				result := matcher.Match(elem)
+				if result.Matched() {
+					why := hamcrest.NewDescription(
+						"Matched element %v of %v: %v", i+1, len(keys), elem)
+					return hamcrest.NewResult(true, why)
+				}
+			}
+			why := hamcrest.NewDescription(
+				"Matched none of the %v elements", len(keys))
+			return hamcrest.NewResult(false, why)
+		}
+		return matcher.Match(v)
+	}
+	return hamcrest.NewMatcher(description, match)
+}
+
+func EveryMapElement(matcher *hamcrest.Matcher) *hamcrest.Matcher {
+	description := hamcrest.NewDescription("EveryMapElement[%v]", matcher)
+	match := func(actual interface{}) *hamcrest.Result {
+		v := reflect.NewValue(actual)
+		if value, ok := v.(*reflect.MapValue); ok {
+			keys := value.Keys()
+			for _, key := range keys {
+				elem := value.Elem(key).Interface()
+				result := matcher.Match(elem)
+				if !result.Matched() {
+					why := hamcrest.NewDescription(
+						"Failed to match map element at key[%v]: %v", key, elem)
+					return hamcrest.NewResult(false, why)
+				}
+			}
+			why := hamcrest.NewDescription(
+				"Matched all of the %v map elements", len(keys))
+			return hamcrest.NewResult(true, why)
+		}
+		return matcher.Match(v)
+	}
+	return hamcrest.NewMatcher(description, match)
+}
+
+
+
---
+++ collections/collections_test.go
@@ -0,0 +1,76 @@
+package collections
+
+import (
+	"hamcrest"
+	"hamcrest/asserter"
+	"hamcrest/comparison"
+	//"reflect"
+	//"runtime"
+	"testing"
+)
+
+var EqualTo = hamcrest.EqualTo
+var Not = hamcrest.Not
+var Anything = hamcrest.Anything
+var LessThan = comparison.LessThan
+var GreaterThan = comparison.GreaterThan
+
+func Test_AnyElement_ofArray(t *testing.T) {
+	we := asserter.Using(t)
+	we.CheckThat([3]int{1, 2, 3}, AnyElement(EqualTo(1)).Comment("first element"))
+	we.CheckThat([3]int{1, 2, 3}, AnyElement(EqualTo(2)).Comment("middle element"))
+	we.CheckThat([3]int{1, 2, 3}, AnyElement(EqualTo(3)).Comment("last element"))
+	we.CheckThat([3]int{1, 2, 3}, Not(AnyElement(EqualTo(4))).Comment("none matching"))
+	we.CheckThat([0]int{}, Not(AnyElement(Anything())).Comment("no elements"))
+}
+
+func Test_EveryElement_ofArray(t *testing.T) {
+	we := asserter.Using(t)
+	we.CheckThat([3]int{1, 2, 3}, EveryElement(LessThan(4)).Comment("all elements"))
+	we.CheckThat([3]int{2, 1, 1}, Not(EveryElement(LessThan(2))).Comment("all but first"))
+	we.CheckThat([3]int{1, 2, 1}, Not(EveryElement(LessThan(2))).Comment("all but middle"))
+	we.CheckThat([3]int{1, 1, 2}, Not(EveryElement(LessThan(2))).Comment("all but last"))
+	we.CheckThat([0]int{}, EveryElement(Anything()).Comment("no elements"))
+}
+
+func Test_AnyElement_ofSlice(t *testing.T) {
+	we := asserter.Using(t)
+	we.CheckThat([]int{1, 2, 3}, AnyElement(EqualTo(1)).Comment("first element"))
+	we.CheckThat([]int{1, 2, 3}, AnyElement(EqualTo(2)).Comment("middle element"))
+	we.CheckThat([]int{1, 2, 3}, AnyElement(EqualTo(3)).Comment("last element"))
+	we.CheckThat([]int{1, 2, 3}, Not(AnyElement(EqualTo(4))).Comment("none matching"))
+	we.CheckThat([]int{}, Not(AnyElement(Anything())).Comment("no elements"))
+}
+
+func Test_EveryElement_ofSlice(t *testing.T) {
+	we := asserter.Using(t)
+	we.CheckThat([]int{1, 2, 3}, EveryElement(LessThan(4)).Comment("all elements"))
+	we.CheckThat([]int{2, 1, 1}, Not(EveryElement(LessThan(2))).Comment("all but first"))
+	we.CheckThat([]int{1, 2, 1}, Not(EveryElement(LessThan(2))).Comment("all but middle"))
+	we.CheckThat([]int{1, 1, 2}, Not(EveryElement(LessThan(2))).Comment("all but last"))
+	we.CheckThat([]int{}, EveryElement(Anything()).Comment("no elements"))
+}
+
+func Test_AnyMapElement(t *testing.T) {
+	we := asserter.Using(t)
+	twoMap := map[string]int{ "foo": 1, "bar": 2 }
+	emptyMap := map[string]int{}
+	we.CheckThat(twoMap, AnyMapElement(EqualTo(1)).Comment("entry [foo: 1]"))
+	we.CheckThat(twoMap, AnyMapElement(EqualTo(2)).Comment("entry [bar: 2]"))
+	we.CheckThat(twoMap, Not(AnyMapElement(EqualTo(3))).Comment("neither entry matches"))
+	we.CheckThat(emptyMap, Not(AnyMapElement(Anything())).Comment("no entries"))
+}
+
+func Test_EveryMapElement(t *testing.T) {
+	we := asserter.Using(t)
+	twoMap := map[string]int{ "foo": 1, "bar": 2 }
+	emptyMap := map[string]int{}
+	we.CheckThat(twoMap, EveryMapElement(GreaterThan(0)).Comment("all elements"))
+	we.CheckThat(twoMap, Not(EveryMapElement(GreaterThan(1))).Comment("not entry [foo: 1]"))
+	we.CheckThat(twoMap, Not(EveryMapElement(LessThan(2))).Comment("not entry [bar: 2]"))
+	we.CheckThat(emptyMap, EveryMapElement(Anything()).Comment("no entries"))
+}
+
+
+
+
