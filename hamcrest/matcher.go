9a77ff8f33c5eef928221da84015cbcc2a9fb046
Mick Killianey <mk@cs.stanford.edu>
2011-01-24 10:09:46 +0000

    Expand the range of acceptable matcher functions

--- hamcrest/matcher.go
+++ hamcrest/matcher.go
@@ -6,6 +6,7 @@ package hamcrest
 
 import (
 	"fmt"
+	"reflect"
 )
 
 // --------------------------------------------------------------------
@@ -135,14 +136,117 @@ type Matcher struct {
 	match func(v interface{}) *Result
 	comments []interface{}
 }
-// Creates a new Matcher using the given description.
-func NewMatcher(match func(v interface{}) *Result, description SelfDescribing) *Matcher {
+
+// Creates a new Matcher using the given matching function, with the
+// given description.
+//
+// Matching functions must have exactly one input parameter.  Matching
+// functions must return a *Result or a bool as its first output value.
+// If the first output value is a *Result, that must be its only output
+// value.  If the first output value is bool, then that value is used
+// for Result.Matched(), and if present, any second output value is used
+// as the result description.  For example, the following signatures are
+// all legal matcher functions:
+//
+//   func(interface{}) *Result
+//   func(interface{}) bool
+//   func(interface{}) (bool, os.Error)
+//   func(string) *Result
+//   func(...*int) bool
+//   func(...io.Reader) (bool os.Error)
+func NewMatcher(fn interface{}, description SelfDescribing) *Matcher {
+	match := normalizeMatchFunction(fn)
 	return &Matcher{ match: match, description: description }
 }
 
-// Creates a new Matcher using the given format/args as a description.
-func NewMatcherf(match func(v interface{}) *Result, format string, args...interface{}) *Matcher {
+// Creates a new Matcher using the given matching function, with the
+// given format/args as a description.
+func NewMatcherf(fn interface{}, format string, args...interface{}) *Matcher {
+	match := normalizeMatchFunction(fn)
 	return NewMatcher(match, Description(format, args...))
+}
+
+func normalizeMatchFunction(fn interface{}) func(interface{}) *Result {
+	if match, ok := fn.(func(interface{}) *Result); ok {
+		return match
+	}
+	if funcValue, ok := reflect.NewValue(fn).(*reflect.FuncValue); ok {
+		funcType := funcValue.Type().(*reflect.FuncType)
+		numIn := funcType.NumIn()
+		numOut := funcType.NumOut()
+		var constructInputValues func(actual interface{}) ([]reflect.Value, interface{})
+		if numIn == 1 {
+			constructInputValues = func(actual interface{}) (values []reflect.Value, err interface{}) {
+				defer func() {
+					err = recover()
+				}()
+				actualValue := reflect.NewValue(actual)
+				inType := funcType.In(0)
+				if funcType.DotDotDot() {
+					sliceValue := reflect.MakeSlice(inType.(*reflect.SliceType), 1, 1)
+					inputValue := sliceValue.Elem(0)
+					inputValue.SetValue(actualValue)
+					values = []reflect.Value{ sliceValue }
+				} else {
+					inputValue := reflect.MakeZero(inType)
+					inputValue.SetValue(actualValue)
+					values = []reflect.Value{ inputValue }
+				}
+				return
+			}
+		} else {
+			reason := fmt.Sprintf("Can't use %T as a matcher: must have one input, had %v",
+				fn, numIn)
+			panic(reason)
+		}
+		outType0 := funcType.Out(0)
+		var resultType = reflect.Typeof(&Result{})
+		var boolType = reflect.Typeof(true)
+		var interpretOutputValues func(values []reflect.Value) *Result
+		if numOut == 1 && outType0 == resultType {
+			interpretOutputValues = func(values []reflect.Value) (result *Result) {
+				defer func() {
+					if err := recover(); err != nil {
+						result = NewResultf(false,
+							"Error:  expected *Result from %T, got %v", fn, values)
+					}
+				}()
+				result = values[0].Interface().(*Result)
+				return
+			}
+		} else if outType0 == boolType && (numOut == 1 || numOut == 2) {
+			interpretOutputValues = func(values []reflect.Value) (result *Result) {
+				defer func() {
+					if err := recover(); err != nil {
+						result = NewResultf(false,
+							"Error:  expected bool from %T, got %v", fn, values)
+					}
+				}()
+				b := values[0].Interface().(bool)
+				if len(values) > 1 {
+					result = NewResultf(b, "%v", values[1].Interface())
+				} else if b {
+					result = NewResultf(true, "Matched")
+				} else {
+					result = NewResultf(false, "Did not match")
+				}
+				return
+			}
+		} else {
+			panic(fmt.Sprintf("Can't use %T as a matcher function", fn))
+		}
+		return func(actual interface{}) *Result {
+			inputValues, problem := constructInputValues(actual)
+			if problem != nil {
+				return NewResultf(false, "Could not apply %T to input of type %T: %v",
+					fn, actual, problem)
+			}
+			outputValues := funcValue.Call(inputValues)
+			return interpretOutputValues(outputValues)
+		}
+	
+	}
+	return nil
 }
 
 // Implementation of SelfDescribing: fmt.Formatter.
--- hamcrest/matcher_test.go
+++ hamcrest/matcher_test.go
@@ -5,6 +5,8 @@
 package hamcrest
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -16,30 +18,53 @@ func Test_Description(t *testing.T) {
 	}
 }
 
-func checkResultIsMatching(t *testing.T, result *Result, message string) {
-	if !result.Matched() {
-		t.Errorf("Expected matching result from applying %v to %#v, was [%v] %v",
-			result.Matcher(), result.Value(), result, message)
-	}
+func attemptMatch(matcher *Matcher, input interface{}) (result *Result, err interface{}) {
+	defer func() {
+		if err = recover(); err != nil {
+			result = nil
+		}
+	}()
+	result = matcher.Match(input)
+	return
 }
 
-func checkResultIsNonMatching(t *testing.T, result *Result, message string) {
+func checkResultIsMatching(t *testing.T, matcher *Matcher, input interface{}, message string) *Result {
+	result, err := attemptMatch(matcher, input)
+	if err != nil {
+		t.Errorf("Expected matching result from applying %v to %#v, was panic [%v]",
+			matcher, input, err)
+		return nil
+	}
+	if !result.Matched() {
+		t.Errorf("Expected matching result from applying %v to %#v, was [%v] %v",
+			matcher, input, result, message)
+	}
+	return result
+}
+
+func checkResultIsNonMatching(t *testing.T, matcher *Matcher, input interface{}, message string) *Result {
+	result, err := attemptMatch(matcher, input)
+	if err != nil {
+		t.Errorf("Expected non-matching result from applying %v to %#v, was panic [%v]",
+			matcher, input, err)
+		return nil
+	}
 	if result.Matched() {
 		t.Errorf("Expected non-matching result from applying %v to %#v, was [%v].  Message: %v",
 			result.Matcher(), result.Value(), result, message)
 	}
+	return result
 }
-
 
 func Test_Matched(t *testing.T) {
 	matcher := Matched()
 	passResult := NewResultf(true, "pass")
 	failResult := NewResultf(false, "fail")
 	
-	checkResultIsMatching(t, matcher.Match(passResult), "matching")
-	checkResultIsNonMatching(t, matcher.Match(failResult), "non-matching")
-	checkResultIsNonMatching(t, matcher.Match(nil), "nil")
-	checkResultIsNonMatching(t, matcher.Match("foo"), "not a Result")
+	checkResultIsMatching(t, matcher, passResult, "matching")
+	checkResultIsNonMatching(t, matcher, failResult, "non-matching")
+	checkResultIsNonMatching(t, matcher, nil, "nil")
+	checkResultIsNonMatching(t, matcher, "foo", "not a Result")
 }
 
 func Test_DidNotMatch(t *testing.T) {
@@ -47,9 +72,107 @@ func Test_DidNotMatch(t *testing.T) {
 	passResult := NewResultf(true, "pass")
 	failResult := NewResultf(false, "fail")
 	
-	checkResultIsNonMatching(t, matcher.Match(passResult), "matching")
-	checkResultIsMatching(t, matcher.Match(failResult), "non-matching")
-	checkResultIsNonMatching(t, matcher.Match(nil), "nil")
-	checkResultIsNonMatching(t, matcher.Match("foo"), "not a Result")
+	checkResultIsNonMatching(t, matcher, passResult, "matching")
+	checkResultIsMatching(t, matcher, failResult, "non-matching")
+	checkResultIsNonMatching(t, matcher, nil, "nil")
+	checkResultIsNonMatching(t, matcher, "foo", "not a Result")
 }
+
+func Test_NewMatcher_Func_InterfaceIn_ResultOut(t *testing.T) {
+	matcherName := "foo matcher"
+	trueMessage := "bar true"
+	falseMessage := "baz false"
+	function := func(v interface{}) *Result {
+		if b, ok := v.(bool); b && ok {
+			return NewResultf(true, trueMessage)
+		}
+		return NewResultf(false, falseMessage)
+	}
+	matcher := NewMatcherf(function, matcherName)
+	if s := fmt.Sprint(matcher); !strings.Contains(s, matcherName) {
+		t.Fatalf("String should have contained %v, but was: %v", matcherName, s)
+	}
+	
+	if result := matcher.Match(true); !result.Matched() {
+		t.Fatalf("Result should have matched, but was: %v", result)
+	} else if s := fmt.Sprint(result); !strings.Contains(s, trueMessage) {
+		t.Fatalf("String should have contained %v, but was: %v", trueMessage, s)
+	}
+	if result := matcher.Match(false);  result.Matched() {
+		t.Fatalf("Result should not have matched, but did: %v", result)
+	} else if s := fmt.Sprint(result); !strings.Contains(s, falseMessage) {
+		t.Fatalf("String should have contained %v, but was: %v", falseMessage, s)
+	}
+}
+
+func Test_NewMatcher_Func_StringIn_BoolOut(t *testing.T) {
+	matcherName := "foo matcher"
+	trueMessage := "Matched"
+	falseMessage := "Did not match"
+	errorMessage := "Could not apply"
+	function := func(s string) bool {
+		if s == "bar" {
+			return true
+		}
+		return false
+	}
+	matcher := NewMatcherf(function, matcherName)
+	if s := fmt.Sprint(matcher); !strings.Contains(s, matcherName) {
+		t.Fatalf("String should have contained %v, but was: %v", matcherName, s)
+	}
+	
+	if result := matcher.Match("bar"); !result.Matched() {
+		t.Fatalf("Result should have matched, but was: %v", result)
+	} else if s := fmt.Sprint(result); !strings.Contains(s, trueMessage) {
+		t.Fatalf("String should have contained %v, but was: %v", trueMessage, s)
+	}
+	if result := matcher.Match("foo");  result.Matched() {
+		t.Fatalf("Result should not have matched, but did: %v", result)
+	} else if s := fmt.Sprint(result); !strings.Contains(s, falseMessage) {
+		t.Fatalf("String should have contained %v, but was: %v", falseMessage, s)
+	}
+	if result := matcher.Match(39);  result.Matched() {
+		t.Fatalf("Result should not have matched, but did: %v", result)
+	} else if s := fmt.Sprint(result); !strings.Contains(s, errorMessage) {
+		t.Fatalf("String should have contained %v, but was: %v", errorMessage, s)
+	}
+}
+
+
+func Test_NewMatcher_Func_StringDotDotDotIn_BoolStringOut(t *testing.T) {
+	matcherName := "foo matcher"
+	trueMessage := "bar true"
+	falseMessage := "baz false"
+	errorMessage := "Could not apply"
+	function := func(s... string) (bool, string) {
+		if len(s) >= 1 && s[0] == "bar" {
+			return true, trueMessage
+		}
+		return false, falseMessage
+	}
+	matcher := NewMatcherf(function, matcherName)
+	if s := fmt.Sprint(matcher); !strings.Contains(s, matcherName) {
+		t.Fatalf("String should have contained %v, but was: %v", matcherName, s)
+	}
+	
+	if result := matcher.Match("bar"); !result.Matched() {
+		t.Fatalf("Result should have matched, but was: %v", result)
+	} else if s := fmt.Sprint(result); !strings.Contains(s, trueMessage) {
+		t.Fatalf("String should have contained %v, but was: %v", trueMessage, s)
+	}
+	if result := matcher.Match("foo");  result.Matched() {
+		t.Fatalf("Result should not have matched, but did: %v", result)
+	} else if s := fmt.Sprint(result); !strings.Contains(s, falseMessage) {
+		t.Fatalf("String should have contained %v, but was: %v", falseMessage, s)
+	}
+	if result := matcher.Match(39);  result.Matched() {
+		t.Fatalf("Result should not have matched, but did: %v", result)
+	} else if s := fmt.Sprint(result); !strings.Contains(s, errorMessage) {
+		t.Fatalf("String should have contained %v, but was: %v", errorMessage, s)
+	}
+}
+
+
+
+
 
